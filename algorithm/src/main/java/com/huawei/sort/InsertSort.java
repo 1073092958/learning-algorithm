150155263ed7a233d71b3dd65e308fbf31dc351a
youyayaya <youyayaya_li@qq.com>
2021-01-14 22:55:02 +0800

    创建项目

---
+++ algorithm/src/main/java/com/huawei/sort/InsertSort.java
@@ -0,0 +1,10 @@
+package com.huawei.sort;
+
+/**
+ * 插入排序
+ *
+ * @since 2021/1/9
+ * @author YOUYAYAYA
+ */
+public class InsertSort {
+}
---
+++ algorithm/src/test/java/com/huawei/compare/TestStudent.java
@@ -0,0 +1,32 @@
+package com.huawei.compare;
+
+/**
+ * 测试Student类实现Comparable接口
+ *
+ * @since 2021/1/11
+ * @author YOUYAYAYA
+ */
+public class TestStudent {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setUsername("张三");
+        student1.setAge(11);
+
+        Student student2 = new Student();
+        student2.setUsername("李四");
+        student2.setAge(22);
+
+        System.out.println(getMax(student1, student2));
+    }
+
+    public static Comparable getMax(Comparable v1, Comparable v2) {
+        int result = v1.compareTo(v2);
+        if (result >= 0) {
+            return v1;
+        } else {
+            return v2;
+        }
+
+    }
+}
